6fa8521980eae5145e813763634ccab212536743
RdShb <105648715+RdShb@users.noreply.github.com>
2022-06-21 12:21:54 -0300

    Add files via upload

---
+++ test/src/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    public static void main(String[] args) throws Exception {
+        int result;
+        result = suma(1, 2, 3);
+        System.out.println(result);
+    }
+
+    public static int suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+        return resultado;
+    }
+}
